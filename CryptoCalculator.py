9c5948e01a8f11477c1d8d927e546861994ce5ea
Omar <omohammed7869@gmail.com>
2017-11-29 14:58:34 -0600

    identified where the error lies, on line 23

--- CryptoCalculator.py
+++ CryptoCalculator.py
@@ -7,17 +7,37 @@ def calculate():
     investment = int(input())
     print("At was price?")
     initialPrice = int(input())
-    print("What is the current price of bitcoin")
+    print("What is the current price of bitcoin") #Later on we'll implement a scraper to grab the current price of bitcoin or whichever crypto!
     finalPrice = int(input())
 
     percentage = (initialPrice,finalPrice)
-    
+
+
+
+
+    currValue = percentage * investment
+
+
+    logging.debug(initialPrice)
+    logging.debug(finalPrice)
+    logging.debug(percentage) #Returns a tuple, which we don't want!
+    logging.debug(currValue)
+
+    #if(currValue > investment):
+       # print("You're current bitcoin is worth: " + currValue + " currently a net profit")
+    #elif(currValue == investment):
+      #  print("You're current bitcoin is worth: " + currValue + " You broke even!")
+    #else:
+     #   print("You're current bitcoin is worth: " + currValue + "currently a net loss")
+
+
 
 
 
 def percentage(initalPrice,finalPrice):
     decrease = float(finalPrice) - float(initalPrice)
-    percentage = (decrease/initalPrice) * 100.0
+    percentage = (decrease/initalPrice) * 100.0 # I have to fix the math here, i assume!
     return percentage
 
-logging.debug(percentage(9784,10094))
+
+logging.debug(calculate())
