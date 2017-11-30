cdd1b4e5e4ce8c8b8b914c111f8a7084f36ed773
Omar <omohammed7869@gmail.com>
2017-11-29 22:25:17 -0600

    Beta verision of calculationsv2()

--- CryptoCalculator.py
+++ CryptoCalculator.py
@@ -3,6 +3,9 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
+
+
+
 def calculateV2():
     # user input a number
     print("Enter number of investments you made")
@@ -19,9 +22,21 @@ def calculateV2():
         investmentList.append(investment)
         count += 1
 
+    #get the average from that list
+    aveInvestment = getAverage(investmentList)
+    print(
+        "What is the current price of Bitcoin?")  # Later on we'll implement a scraper to grab the current price of bitcoin or whichever crypto!
+    finalPrice = int(input())
 
-# logging.debug(calculateV2())
+    percentage = findPercentage(aveInvestment,finalPrice)
+    currValue = ((percentage) * investment) + investment
 
+    if (currValue > investment):
+        print("Your current bitcoin is worth: ${:,.2f}. Currently a net profit!".format(float(currValue)))
+    elif (currValue == investment):
+        print("Your current bitcoin is worth: ${:,.2f}. You broke even.".format(float(currValue)))
+    else:
+        print("Your current bitcoin is worth: ${:,.2f}. Currently a net loss.".format(float(currValue)))
 
 def calculate():
     print("Hello! How much money did you invest into Bitcoin?")
@@ -45,25 +60,15 @@ def calculate():
 
 def findPercentage(iPrice, fPrice):
     decrease = float(fPrice) - float(iPrice)
-    perc = float(decrease / iPrice)  # I have to fix the math here, i assume!
+    perc = float(decrease / iPrice)  # I have to fix the math here, I assume!
     return perc
 
-
 def getAverage(*list):
-    sum = 0
-    counter = 0
-    for x in list:
-        counter += 1
-        sum += x
-    return sum / counter
+    ave = sum(*list)/len(*list)
+    return ave
 
-    # logging.debug(getAverage(5,66,2))
+logging.debug(calculateV2())
 
 
 
-<<<<<<< HEAD
-    # calculate()
-=======
-#calculate()
 
->>>>>>> 62817d21b344aeb5e61e67374629c02977f428cd
